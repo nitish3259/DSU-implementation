0bfd6871e6b66652d7bd77d3fdc3637c6d747cd3
nitish3259 <67655466+nitish3259@users.noreply.github.com>
2020-08-29 21:06:36 +0530

    CREATE main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+# DSU-implementation
+
+//EXAMPLE IMPLEMENTATION
+
+#include<iostream>
+using namespace std;
+#include <vector>
+#include"DSU.h"
+
+
+int main(){
+    DSU myDSU(4);
+    myDSU.MakeSet();
+    myDSU.MakeSet();
+    myDSU.Union(1,2);
+    myDSU.Union(4,3);
+    cout<<myDSU.Find(5);
+}
